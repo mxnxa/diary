1594233be0b07e0647a9a750773fcef87d9a7605
mxnxa <alsgk0428@naver.com>
2023-02-18 20:29:42 +0900

    editor 수정

--- src/components/write/daily/Editor.js
+++ src/components/write/daily/Editor.js
@@ -5,18 +5,24 @@ import "react-quill/dist/quill.snow.css";
 import Responsive from "../../common/Responsive";
 import WriteActionButton from "../WriteActionButton";
 
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const EditorBlock = styled(Responsive)`
   border-radius: 20px;
-  margin-top: 100px;
-  margin-bottom: 100px;
-  padding: 50px;
+  padding: 40px 60px;
   background-color: #fff;
   box-shadow: 0 3px 4px rgba(0, 0, 0, 0.08);
 `;
 
 const DateBlock = styled.div`
   text-align: center;
-  margin-bottom: 60px;
+  margin-bottom: 40px;
 
   p {
     display: inline-block;
@@ -28,12 +34,21 @@ const StyledInput = styled.input`
   width: 100%;
   border: none;
   outline: none;
-  margin-bottom: 20px;
+  padding: 12px 15px;
 `;
 
 const TitleInput = styled(StyledInput)`
-  font-size: 40px;
+  font-size: 30px;
   font-weight: 700;
+  border: 1px solid #ccc;
+  border-bottom: none;
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    min-height: 500px;
+    font-size: 16px;
+  }
 `;
 
 const today = {
@@ -48,13 +63,15 @@ function Editor() {
   const { year, month, date, hour, minute } = today;
   const h12 = hour > 12 ? hour - 12 : hour;
   const ampm = hour < 12 ? "오전" : "오후";
-  const todaytime = `${year}.${month}.${date} ${ampm} ${h12}:${minute}`;
+  const min_0 = minute < 10 ? "0" + minute : minute;
+  const todaytime = `${year}.${month}.${date} ${ampm} ${h12}:${min_0}`;
 
   const modules = {
     toolbar: {
       container: [
-        [{ size: ["small", false, "large", "huge"] }, { color: [] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ font: [] }, { size: ["small", false, "large", "huge"] }],
+        ["bold", "italic", "underline", "strike"],
+        [{ color: [] }, { background: [] }],
         [
           { list: "ordered" },
           { list: "bullet" },
@@ -62,21 +79,30 @@ function Editor() {
           { indent: "+1" },
           { align: [] },
         ],
-        ["code-block", "link", "image"],
+        [{ header: "1" }, { header: "2" }, "blockquote", "code-block"],
+        ["link", "image", "video"],
       ],
     },
   };
 
   return (
-    <EditorBlock>
-      <DateBlock>
-        <p> {todaytime}</p>
-        {/* {year}.{month}.{date} {ampm} {hour}:{minute} */}
-      </DateBlock>
-      <TitleInput placeholder="제목" />
-      <ReactQuill theme="snow" modules={modules} />
-      <WriteActionButton save />
-    </EditorBlock>
+    <Wrapper>
+      <EditorBlock>
+        <DateBlock>
+          <p> {todaytime}</p>
+          {/* {year}.{month}.{date} {ampm} {hour}:{minute} */}
+        </DateBlock>
+        <TitleInput placeholder="제목" />
+        <QuillWrapper>
+          <ReactQuill
+            theme="snow"
+            modules={modules}
+            placeholder="소중한 일상을 기록해보세요."
+          />
+        </QuillWrapper>
+        <WriteActionButton save />
+      </EditorBlock>
+    </Wrapper>
   );
 }
 
--- src/components/write/schedule/Editor.js
+++ src/components/write/schedule/Editor.js
@@ -4,12 +4,18 @@ import palette from "../../../lib/styles/palette";
 import Responsive from "../../common/Responsive";
 import WriteActionButton from "../WriteActionButton";
 
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const EditorBlock = styled(Responsive)`
-  /* height: 100vh; */
+  height: 80%;
   border-radius: 20px;
-  margin-top: 100px;
-  /* margin-bottom: 100px; */
-  padding: 50px;
+  padding: 40px 60px;
   background-color: #fff;
   box-shadow: 0 3px 4px rgba(0, 0, 0, 0.08);
 `;
@@ -46,20 +52,22 @@ const SelectDateBlock = styled.div`
 
 function Editor() {
   return (
-    <EditorBlock>
-      <Title>일정 추가</Title>
-      <StyledInput placeholder="제목" />
-      <SelectDateBlock>
-        <div className="start">
-          시작 <p>2023. 02. 17 오후 9:31</p>
-        </div>
-        <div className="finish">
-          종료 <p>2023. 02. 17 오후 9:31</p>
-        </div>
-      </SelectDateBlock>
-      <StyledInput placeholder="메모" />
-      <WriteActionButton />
-    </EditorBlock>
+    <Wrapper>
+      <EditorBlock>
+        <Title>일정 추가</Title>
+        <StyledInput placeholder="제목" />
+        <SelectDateBlock>
+          <div className="start">
+            시작 <p>2023. 02. 17 오후 9:31</p>
+          </div>
+          <div className="finish">
+            종료 <p>2023. 02. 17 오후 9:31</p>
+          </div>
+        </SelectDateBlock>
+        <StyledInput placeholder="메모" />
+        <WriteActionButton />
+      </EditorBlock>
+    </Wrapper>
   );
 }
 
